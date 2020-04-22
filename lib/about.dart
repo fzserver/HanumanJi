07daf365a46d41ddf29b1d596213a3974630bc07
FrazileDevelopers <frazile.com@gmail.com>
2020-04-22 05:36:46 -0700

    Bugs Fixed

--- lib/about.dart
+++ lib/about.dart
@@ -5,46 +5,57 @@ class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.yellow,
         appBar: AppBar(
-          title: Text('About'),
+          iconTheme: IconThemeData(
+            color: Colors.deepOrange,
+          ),
+          title: Text(
+            'About',
+            style: TextStyle(
+              color: Colors.deepOrange,
+            ),
+          ),
         ),
         body: aboutBody());
   }
 }
 
 Widget aboutBody() => SingleChildScrollView(
-        child: Card(
-            child: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          CircleAvatar(
-            radius: 100.0,
-            backgroundImage: AssetImage('hanuman.png'),
+    child: Card(
+        color: Colors.yellow,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FadeInImage(
+                image: AssetImage('hanuman.png'),
+                placeholder: AssetImage('hanuman.png'),
+                fit: BoxFit.contain,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Hanuman Ji',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrange,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Hanuman Chalisa a hindu prayer of Goddess Hanuman Ji to think only for Goddess Ram while praying this prayer.',
+                style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.deepOrange),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            'Hanuman Ji',
-            style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.w300,
-              color: Colors.pinkAccent,
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            'This is the wallpapers app whole users waiting for.',
-            style: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.black),
-          ),
-        ],
-      ),
-    )));
+        )));
--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:HanumanChalisa/prayer.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_admob/firebase_admob.dart';
-import 'package:share/share.dart';
-import 'package:launch_review/launch_review.dart';
 import 'dart:io' show Platform;
 
 void main() {
@@ -20,18 +18,93 @@ void main() {
 enum PlayerStatus { Playing, Paused, Stop, Resume }
 
 class Hanuman extends StatefulWidget {
+  static FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: firebaseAnalytics);
+
+  // Admob Implementation
+  static final MobileAdTargetingInfo targetInfo = MobileAdTargetingInfo(
+    testDevices: <String>["52161986C14504D2EE7019AAC96D045E"],
+    keywords: <String>[
+      'WALLPAPERS',
+      'WALLS',
+      'AMOLED',
+      'Hanuman',
+      'God',
+      'Goddess',
+      'Prayer',
+      'Ram',
+    ],
+    childDirected: true,
+  );
+
   @override
-  _HanumanState createState() => _HanumanState();
+  HanumanState createState() {
+    return new HanumanState();
+  }
 }
 
-class _HanumanState extends State<Hanuman> {
+class HanumanState extends State<Hanuman> {
+  BannerAd _bannerAd;
+
+  String bannerAdUnitId = Platform.isIOS
+      ? 'ca-app-pub-7600031190372955/9354587981'
+      : 'ca-app-pub-7600031190372955/8284446258';
+
+  BannerAd createBannerAd() => BannerAd(
+      adUnitId: bannerAdUnitId,
+      size: AdSize.smartBanner,
+      targetingInfo: Hanuman.targetInfo,
+      listener: (MobileAdEvent event) {
+        print("Banner event : $event");
+      });
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAdMob.instance.initialize(
+        appId: Platform.isIOS
+            ? 'ca-app-pub-7600031190372955~9988356267'
+            : 'ca-app-pub-7600031190372955~4536772939');
+    _bannerAd = createBannerAd()..load();
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Hanuman Chalisa',
+        navigatorObservers: <NavigatorObserver>[Hanuman.observer],
+        theme: ThemeData(
+          primaryColor: Colors.yellowAccent,
+          accentColor: Colors.deepOrange,
+        ),
+        builder: (BuildContext context, Widget widget) {
+          _bannerAd..show();
+
+          // var mediaQuery = MediaQuery.of(context);
+          double paddingBottom = 50.0;
+          double paddingRight = 0.0;
+
+          return Padding(
+            child: widget,
+            padding:
+                EdgeInsets.only(bottom: paddingBottom, right: paddingRight),
+          );
+        },
+        home: HanumanChalisa(),
+      );
+}
+
+class HanumanChalisa extends StatefulWidget {
+  @override
+  _HanumanChalisaState createState() => _HanumanChalisaState();
+}
+
+class _HanumanChalisaState extends State<HanumanChalisa> {
   AudioPlayer audioPlayer = AudioPlayer();
   int playaudio;
   String prayer;
   String prayerurl;
-  static FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics();
-  static FirebaseAnalyticsObserver observer =
-      FirebaseAnalyticsObserver(analytics: firebaseAnalytics);
 
   int currentPlaying;
   PlayerStatus _playerStatus;
@@ -57,55 +130,21 @@ class _HanumanState extends State<Hanuman> {
     if (_playerStatus == PlayerStatus.Playing) {
       return Icon(
         Icons.pause,
-        color: Colors.white,
+        color: Colors.deepOrange,
       );
     }
     return Icon(
       Icons.play_arrow,
-      color: Colors.white,
+      color: Colors.deepOrange,
     );
   }
-
-  // Admob Implementation
-  static final MobileAdTargetingInfo targetInfo = MobileAdTargetingInfo(
-    testDevices: <String>["52161986C14504D2EE7019AAC96D045E"],
-    keywords: <String>[
-      'WALLPAPERS',
-      'WALLS',
-      'AMOLED',
-      'Hanuman',
-      'God',
-      'Goddess',
-      'Prayer',
-      'Ram',
-    ],
-    childDirected: true,
-  );
-
-  BannerAd _bannerAd;
-  String bannerAdUnitId = Platform.isIOS
-      ? 'ca-app-pub-7600031190372955/9354587981'
-      : 'ca-app-pub-7600031190372955/8284446258';
-
-  BannerAd createBannerAd() => BannerAd(
-      adUnitId: bannerAdUnitId,
-      size: AdSize.smartBanner,
-      targetingInfo: targetInfo,
-      listener: (MobileAdEvent event) {
-        print("Banner event : $event");
-      });
 
   // init State
   @override
   void initState() {
     super.initState();
-    FirebaseAdMob.instance.initialize(
-        appId: Platform.isIOS
-            ? 'ca-app-pub-7600031190372955~9988356267'
-            : 'ca-app-pub-7600031190372955~4536772939');
     prayer = hanumanprayer();
     prayerurl = hanumanaudio();
-    _bannerAd = createBannerAd()..load();
     playaudio = 0;
     currentPlaying = -1;
     _playerStatus = PlayerStatus.Stop;
@@ -114,117 +153,102 @@ class _HanumanState extends State<Hanuman> {
   // dispose
   @override
   void dispose() {
-    _bannerAd?.dispose();
+    // _bannerAd?.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Hanuman Chalisa',
-        navigatorObservers: <NavigatorObserver>[observer],
-        theme: ThemeData(
-          primaryColor: Color.fromRGBO(58, 66, 86, 1.0),
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.deepOrange,
+          ),
+          title: Text(
+            'Hanuman Chalisa',
+            style: TextStyle(color: Colors.deepOrange),
+          ),
+          elevation: 1.0,
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                color: Colors.deepOrange,
+              ),
+              onPressed: () => {},
+            ),
+            PopupMenuButton<MenuItems>(
+              elevation: 3.0,
+              onCanceled: () => {},
+              tooltip: "Menu",
+              onSelected: selectedMenuItem,
+              itemBuilder: (BuildContext context) {
+                return menu.map((MenuItems menuItem) {
+                  return PopupMenuItem<MenuItems>(
+                    value: menuItem,
+                    child: Text(menuItem.title),
+                  );
+                }).toList();
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 2.0),
+            ),
+          ],
         ),
-        builder: (BuildContext context, Widget widget) {
-          _bannerAd..show();
-
-          // var mediaQuery = MediaQuery.of(context);
-          double paddingBottom = 50.0;
-          double paddingRight = 0.0;
-
-          return Padding(
-            child: widget,
-            padding:
-                EdgeInsets.only(bottom: paddingBottom, right: paddingRight),
-          );
-        },
-        home: Scaffold(
-          backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-          appBar: AppBar(
-            title: Text(
-              'Hanuman Chalisa',
-              style: TextStyle(color: Colors.deepOrange),
-            ),
-            elevation: 1.0,
-            centerTitle: true,
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.share),
-                onPressed: () => {},
-              ),
-              PopupMenuButton<MenuItems>(
-                elevation: 3.0,
-                onCanceled: () => {},
-                tooltip: "Menu",
-                onSelected: selectedMenuItem,
-                itemBuilder: (BuildContext context) {
-                  return menu.map((MenuItems menuItem) {
-                    return PopupMenuItem<MenuItems>(
-                      value: menuItem,
-                      child: Text(menuItem.title),
-                    );
-                  }).toList();
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 2.0),
-              ),
-            ],
-          ),
-          body: Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: Card(
-                      color: Color.fromRGBO(58, 66, 86, .9),
-                      child: SafeArea(
-                        child: Hero(
-                          child: FadeInImage(
-                            image: AssetImage('hanumanji.jpg'),
-                            fit: BoxFit.cover,
-                            placeholder: AssetImage('hanumanji.jpg'),
-                          ),
-                          tag: "Jai Hanuman Ji",
-                        ),
-                      ),
-                    ),
-                  ),
-                  Card(
-                    elevation: 2.0,
-                    color: Color.fromRGBO(58, 66, 86, .9),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Card(
+                    color: Colors.yellow,
                     child: SafeArea(
-                      // child: SingleChildScrollView(
-                      //   scrollDirection: Axis.vertical,
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text('$prayer',
-                            style: TextStyle(
-                                fontSize: 18.0,
-                                color: Colors.deepOrange,
-                                fontWeight: FontWeight.w200)),
+                      child: Hero(
+                        child: FadeInImage(
+                          image: AssetImage('hanumanji.jpg'),
+                          fit: BoxFit.cover,
+                          placeholder: AssetImage('hanumanji.jpg'),
+                        ),
+                        tag: "Jai Hanuman Ji",
                       ),
-                      // ),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 10.0),
-                  )
-                ],
-              ),
+                ),
+                Card(
+                  elevation: 2.0,
+                  color: Colors.yellowAccent,
+                  child: SafeArea(
+                    // child: SingleChildScrollView(
+                    //   scrollDirection: Axis.vertical,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text('$prayer',
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.deepOrange,
+                              fontWeight: FontWeight.w200)),
+                    ),
+                    // ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                )
+              ],
             ),
           ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.deepOrange,
-            foregroundColor: Colors.white,
-            child: playerIcon(),
-            onPressed: () => playpause('$prayerurl'),
-          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.yellow,
+          foregroundColor: Colors.deepOrange,
+          child: playerIcon(),
+          onPressed: () => playpause('$prayerurl'),
         ),
       );
 
